067d39b8f22741215a52e598aaf6306c8de26d3b
Gopal Goyal <51030583+anonymouslyknown@users.noreply.github.com>
2021-06-01 17:35:04 +0530

    adding

--- lib/Screens/welcome_screen.dart
+++ lib/Screens/welcome_screen.dart
@@ -11,66 +11,57 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
+      backgroundColor: Color(0xFFFDF1D5),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Image.asset('assets/images/leaf2.png',
+            scale: 0.5),
+            SizedBox(
+              height: 30.0,
+            ),
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('assets/images/leaf.png'),
-                  height: 60.0,
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                    elevation: 5.0,
+                    color: Colors.lightBlueAccent,
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: MaterialButton(
+                      onPressed: () {
+                        //Go to login screen.
+                      },
+                      minWidth: 100.0,
+                      height: 42.0,
+                      child: Text(
+                        'Log In',
+                      ),
+                    ),
+                  ),
                 ),
-                Text(
-                  'Register Screen',
-                  style: TextStyle(
-                     fontSize: 35.0,
-                    fontWeight: FontWeight.w900,
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Material(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(30.0),
+                    elevation: 5.0,
+                    child: MaterialButton(
+                      onPressed: () {
+                        //Go to registration screen.
+                      },
+                      minWidth: 100.0,
+                      height: 42.0,
+                      child: Text(
+                        'Register',
+                      ),
+                    ),
                   ),
                 ),
               ],
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to registration screen.
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
             ),
           ],
         ),
